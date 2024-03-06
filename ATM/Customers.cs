95ad6b4492fe5dbc6827f471cb38ccb92f709c5e
natalia.giorgobiani1@gmail.com <natalia.giorgobiani1@gmail.com>
2024-03-06 16:46:06 +0400

    4 sakiTxi gasasworebeli maqvs

---
+++ ATM/Customers.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Customers
+    {
+        private const string _fileLocation = "C:\\Users\\madnj\\OneDrive\\Desktop\\natas\\project\\Project_4\\Project_4\\customersdata.json";
+        public static List<Customers> Customer = new();
+        private const string _fileLocationLog = "C:\\Users\\madnj\\OneDrive\\Desktop\\natas\\project\\Project_4\\Project_4\\Log.json";
+
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Password { get; set; }
+        public int Balance { get; set; }
+
+
+
+        public void DepositMoney(int money)
+        {
+            this.Balance += money;
+        }
+
+        public void WithdrawMoney(int money)
+        {
+            this.Balance -= money;
+        }
+
+        public void CheckWallet()
+        {
+            Console.WriteLine($"Account Balance: {this.Balance}");
+        }
+
+
+        public void GeneratePassword()
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                string first = random.Next(0, 9).ToString();
+                string lastThree = random.Next(100, 999).ToString();
+                string finalVersion = first + lastThree;
+                if (!Customer.Select(x => x.Password).Contains(finalVersion))
+                {
+                    Password = finalVersion;
+                    break;
+                }
+
+            }
+
+        }
+
+        public void Registration(Customers model)
+        {
+            model.Id = Customer.Max(x => x.Id) + 1;
+            Customer.Add(model);
+
+        }
+        public static void Read()
+        {
+            var data = File.ReadAllText(_fileLocation);
+            Customer = Parse(data);
+        }
+
+        private static List<Customers> Parse(string input)
+        {
+            var result = JsonSerializer.Deserialize<List<Customers>>(input);
+            return result;
+        }
+
+        private static string ToJson(Customers input)
+        {
+            string result = JsonSerializer.Serialize(input);
+            return result;
+        }
+
+
+        public Customers GetSingleCustomer(int id)
+        {
+            var result = Customer.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+            {
+                throw new NullReferenceException("Data not found");
+            }
+
+            return result;
+        }
+
+        public static void Save(Customers model)
+        {
+
+            if (Customer.Count == 0)
+            {
+                model.Id = 1;
+            }
+            else
+            {
+                model.Id = Customer.Max(x => x.Id) + 1;
+            }
+            Customer.Add(model);
+
+
+
+            string json = JsonSerializer.Serialize(Customer, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_fileLocationLog, json);
+
+        }
+
+        /*public static void SaveOperation(Customers operations)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            operations.Add(Customers.Save);
+            string json = JsonSerializer.Serialize(Operations, options);
+            File.WriteAllText(_fileLocationLog, json);
+        }
+*/
+
+
+    }
+}
---
+++ ATM/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace ATM
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<Customers> records = new List<Customers>();
+            records.Add(new Customers());
+
+
+            Console.WriteLine("Welcome to Tsutski Bank!");
+            Console.WriteLine("Choose bank operation by number:");
+            Console.WriteLine("1 - Login;");
+            Console.WriteLine("2 - Registration;");
+            Console.WriteLine("3 - Exit");
+
+            char input = char.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case '1':
+                    Console.WriteLine("Pleas, Enter your user an password!");
+                    Login(records);
+                    break;
+
+                case '2':
+                    Console.WriteLine("Lets Do it!");
+                    Regist(records);
+                    break;
+                case '3':
+                    Console.WriteLine("Buy!");
+                    break;
+
+            }
+
+
+        }
+        public static void Login(List<Customers> records)
+        {
+            Customers.Read();
+            Customers customers = new Customers();
+            Console.WriteLine("input you idnumber:");
+            string idnumber = Console.ReadLine();
+            Customers existingUser = Customers.Customer.Where(Customers => Customers.IdentityNumber == idnumber).FirstOrDefault();
+            if (idnumber == null)
+            {
+                Console.WriteLine("Check Your input!");
+
+            }
+            Console.WriteLine("Input your Password:");
+            string password = Console.ReadLine();
+            Customers existingPassword = Customers.Customer.Where(Customers => Customers.Password == password).FirstOrDefault();
+
+            if (password != existingUser.Password)
+            {
+                Console.WriteLine("Check your password!");
+            }
+
+            Options(records);
+
+
+        }
+
+
+        public static void Regist(List<Customers> records)
+        {
+            Customers.Read();
+            Customers NewCusromer = new Customers();
+            Customers customers = new Customers();
+
+
+
+            Console.WriteLine("input your FirstName");
+            NewCusromer.FirstName = Console.ReadLine();
+            Console.WriteLine("input your LastName");
+            NewCusromer.LastName = Console.ReadLine();
+            Console.WriteLine("input your IDNumber");
+            NewCusromer.IdentityNumber = Console.ReadLine();
+
+
+            while (NewCusromer.IdentityNumber.Length != 11)
+            {
+                Console.WriteLine("Please Enter 11-digit number:");
+                NewCusromer.IdentityNumber = Console.ReadLine();
+            }
+
+            if (Customers.Customer.Select(x => x.IdentityNumber).Contains(NewCusromer.IdentityNumber))
+            {
+                Console.WriteLine("Already registered with this Identification Number.");
+                return;
+            }
+
+            NewCusromer.GeneratePassword();
+            Console.WriteLine($"Your password is {NewCusromer.Password}");
+            NewCusromer.Balance = 0;
+            Customers.Save(NewCusromer);
+            customers = NewCusromer;
+
+            Options(records);
+
+
+        }
+
+
+        public static void Options(List<Customers> records)
+        {
+            Customers customers = new Customers();
+            while (true)
+            {
+                Console.WriteLine("Choose operations:");
+                Console.WriteLine("1 - Deposit");
+                Console.WriteLine("2-Withdraw");
+                Console.WriteLine("3-Check Balance");
+                Console.WriteLine("4 - Log out");
+                string amount = Console.ReadLine();
+                int money = int.Parse(amount);
+
+
+                char marked = char.Parse(Console.ReadLine());
+                switch (marked)
+                {
+                    case '1':
+
+                        Console.WriteLine("Enter amount of money:");
+                        customers.DepositMoney(money);
+                        Customers.Save(customers);
+                        Console.WriteLine(Customers.Read);
+
+                        break;
+
+                    case '2':
+
+                        Console.WriteLine("Enter amount of money:");
+                        customers.WithdrawMoney(money);
+                        Customers.Save(customers);
+                        break;
+
+                    case '3':
+
+                        customers.CheckWallet();
+                        break;
+
+                    case '4':
+
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
